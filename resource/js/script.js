8cfefdf8dcbd56217dbe83338f0e1439a352955e
Ashraf59 <ashrafams59b@gmail.com>
2022-01-28 01:20:26 +0600

    first commit

---
+++ resource/js/script.js
@@ -0,0 +1,45 @@
+/*=========
+//MixitUp
+===========*/
+
+var mixer = mixitup('.grid-portfolio-container');
+
+/*=========
+//Scrollify
+===========*/
+
+$(function() {
+          $.scrollify({
+            section : ".scrollify",
+              setHeights: false,
+              scrollSpeed: 800,
+              updateHash: false,
+          });
+        });
+
+/*===============
+//Hemburger Menu
+================*/
+
+function openMenu(){
+    document.getElementById('navbar').style.height = "100%";
+}
+
+function closeMenu(){
+    document.getElementById('navbar').style.height = "0%";
+}
+
+/*===============
+//Smooth Scroll
+================*/
+
+$(function(){
+    $('.menu-item a, .scroll-down a').on('click', function(){
+        
+        $('html,body').animate({
+           scrollTop: $($.attr(this, 'href')).offset().top
+        },1000);
+        
+        return false;
+    })
+})
